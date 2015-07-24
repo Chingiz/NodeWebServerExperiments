be0988fd35edc15014121e62d4ea1d3010d7480c
Chingiz <chingiz.h@gmail.com>
2015-07-23 19:25:15 -0700

    second efficient server

---
+++ secondServer.js
@@ -0,0 +1,52 @@
+/**
+ * Created by Chingiz on 7/23/2015.
+ */
+
+var http = require('http'),
+    fs = require('fs'),
+    path = require('path'),
+    host = '127.0.0.1',
+    port = '9000';
+
+var mimes = {
+    ".html" : "text/html",
+    ".css" : "text/css",
+    ".js" : "text/javascript",
+    ".gif" : "image/gif",
+    ".jpg" : "image/jpeg",
+    ".png" : "image/png"
+};
+
+var server  = http.createServer(function(req, res) {
+    var filepath = (req.url === '/') ? ('./index.html') : ('.' + req.url);
+    var contentType = mimes[path.extname(filepath)];
+    // Checking to see if the file exists
+    fs.exists(filepath, function(f_exists) {
+        if (f_exists) {
+            //read and sever
+            //fs.readFile(filepath, function(error, content) {
+            //    if(error) {
+            //        res.writeHead(500);
+            //        res.end();
+            //    } else {
+            //        res.writeHead(200, {"Content-Type": contentType});
+            //        res.end(content, "utf-8");
+            //    }
+            //})
+
+            res.writeHead(200, {"Content-Type": contentType});
+            var streamFile = fs.createReadStream(filepath).pipe(res);
+
+            streamFile.on('error', function() {
+                res.writeHead(500);
+                res.end();
+            });
+
+        } else {
+            res.writeHead(404);
+            res.end("Sorry, error!");
+        }
+    })
+}).listen(port, host, function() {
+    console.log("server is runnig");
+});
